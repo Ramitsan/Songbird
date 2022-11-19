2a894ee758cc9c6680db65c4c127295556d0f1db
Ramitsan <ramitsan@gmail.com>
2022-11-19 03:24:29 +0300

    refactor: remove commented code

--- songbird/src/application/start-screen/startScreen.js
+++ songbird/src/application/start-screen/startScreen.js
@@ -16,18 +16,6 @@ export class StartScreen extends Control {
         const header = new Control(mainWrapper.node, 'header', 'header', '');
         const logo = new Control(header.node, 'a', 'logo', 'SongBird');
        
-        // const nav = new Control(header.node, 'nav', 'main-nav', '');
-        // const navList = new Control(nav.node, 'ul', 'nav-list', '');
-        // const navItemGame = new Control(navList.node, 'li', 'nav-item-game', '');      
-        // const navGameLink = new Control(navItemGame.node, 'a', 'nav-game-link', 'Новая игра');
-        // navGameLink.node.onclick = () => { 
-        //     this.onNewGame();           
-        // };
-        // const navItemGallery = new Control(navList.node, 'li', 'nav-item-gallery', '');
-        // const navGalleryLink = new Control(navItemGallery.node, 'a', 'nav-gallery-link', 'Галерея');
-        // navGalleryLink.node.onclick = () => {  
-        //     this.onGallery();                      
-        // };
         const headerButtonsWrapper = new Control(header.node, 'div', 'header-buttons-wrapper', '');
         const buttonNewGame = new Control(headerButtonsWrapper.node, 'button', 'header-button', 'Новая игра');
         buttonNewGame.node.onclick = () => {
